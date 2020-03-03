099d81175f8716e24fb5361bfb7e8ebf5b13a321
Davide Bolognini <davidebolognini7@gmail.com>
2020-03-03 16:11:18 +0100

    Update fqreader.cpp

--- src/fqreader.cpp
+++ src/fqreader.cpp
@@ -1,11 +1,10 @@
-// compile with g++ -o fqreader fqreader.cpp -lz -std=c++11
-
 #include <iostream>
 #include <string>
 #include <zlib.h>
-
 #include "stdc++.h" 
 #include "kseq.h"
+#include <sdsl/suffix_arrays.hpp>
+using namespace sdsl;
 
 char complement(char n)
 {   
@@ -32,11 +31,11 @@ KSEQ_INIT(gzFile,gzread)
 int main(int argc, char **argv)
 {
 
+  csa_wt<> fmi;
   kseq_t *seq;
   int n = 0;
   int kmer = atoi(argv[2]);
   std::unordered_set<std::string> set;
-
   gzFile fp;
   fp = gzopen(argv[1], "r");
   seq = kseq_init(fp);
@@ -53,29 +52,29 @@ int main(int argc, char **argv)
 
       std::string forw = s.substr(i, kmer);
       set.insert(forw);
-      std::cout << forw << std::endl;
       std::string rev = forw;
       std::transform(rev.rbegin(), rev.rend(), rev.begin(), complement);
       set.insert(rev);
-      std::cout << rev << std::endl;
-
+    
     }
 
   }
+
+  kseq_destroy(seq);
+  gzclose(fp);
+  std::ofstream kmdump("kmers.txt"); 
 
   for (auto s : set)
   
   {
    
-    std::cout << s << " "; 
+    kmdump << s << std::endl;
   
   }
 
-  std::cout << std::endl << "Processed " << n << " sequences" << '\n';
-
-  kseq_destroy(seq);
-  gzclose(fp);
-  
+  std::cout << "Processed " << n << " sequences" << '\n';
+  construct(fmi,"kmers.txt", 1);
+  store_to_file(fmi,"kmers.fm");
 
   return 0;
 }
