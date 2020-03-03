de3f30866c9cce4bd0fa32fac106615fd4078469
davidebolo1993 <davidebolognini7@gmail.com>
2020-03-03 14:12:13 +0100

    commit

--- src/fqreader.cpp
+++ src/fqreader.cpp
@@ -3,9 +3,28 @@
 #include <iostream>
 #include <string>
 #include <zlib.h>
-#include "stdc++.h" 
 
+#include "stdc++.h" 
 #include "kseq.h"
+
+char complement(char n)
+{   
+    switch(n)
+    {   
+    case 'A':
+      return 'T';
+    case 'T':
+      return 'A';
+    case 'G':
+      return 'C';
+    case 'C':
+      return 'G';
+    case 'N':
+      return 'N';  
+    default:
+      throw std::domain_error("Invalid nucleotide.");
+    }
+}   
 
 
 KSEQ_INIT(gzFile,gzread)
@@ -15,30 +34,36 @@ int main(int argc, char **argv)
 
   kseq_t *seq;
   int n = 0;
-  int kmer = 3;
+  int kmer = atoi(argv[2]);
   std::unordered_set<std::string> set;
 
   gzFile fp;
   fp = gzopen(argv[1], "r");
   seq = kseq_init(fp);
   while (kseq_read(seq) >= 0){
+    
     ++n;
-
     std::string s = seq->seq.s;
+    std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+    
     std::string q = seq->qual.s;
 
     for (int i = 0; i <= s.length() - kmer; i++) {
 
-      std::string subs = s.substr(i, kmer);
-      set.insert(subs);
+      std::string forw = s.substr(i, kmer);
+      set.insert(forw);
+      std::cout << forw << std::endl;
+      std::string rev = forw;
+      std::transform(rev.rbegin(), rev.rend(), rev.begin(), complement);
+      set.insert(rev);
+      std::cout << rev << std::endl;
 
     }
-
-    std::cout << s << std::endl << q << std::endl;
 
   }
 
   for (auto s : set)
+  
   {
    
     std::cout << s << " "; 
